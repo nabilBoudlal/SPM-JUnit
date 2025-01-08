eaea7f71293a75ec71cf284e9a2bcb8ddb67119e
nabil <nabil.boudlal@studenti.unicam.it>
2025-01-08 10:31:30 +0100

    Acceptance test created

---
+++ src/test/java/org/example/ChromeTest.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Disabled
+    @Test
+    void test() throws InterruptedException {
+        // Exercise
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+         Thread.sleep(3000);
+        // Verify
+        assertTrue(title.contains("Selenium WebDriver"), "The string does contain the expected substring");
+    }
+
+    @Test
+    void testApp(){
+        driver.get("http://localhost:8080/MavenTest2/");
+        String title = driver.getTitle();
+        // Verify
+        assertTrue(title.contains("SPM 2025"), "The string does contain the expected substring");
+    }
+
+}
